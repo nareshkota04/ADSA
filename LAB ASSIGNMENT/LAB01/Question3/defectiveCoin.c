33480f6fc3e749188acba93052ed1bd7e30ba3a9
nareshkota04 <nareshkota0931@gmail.com>
2025-09-02 22:48:13 +0530

    Update defectiveCoin.c

--- LAB ASSIGNMENT/LAB01/Question3/defectiveCoin.c
+++ LAB ASSIGNMENT/LAB01/Question3/defectiveCoin.c
@@ -1,48 +1,48 @@
 #include <stdio.h>
-int weight(int coins[], int left1, int right1, int left2, int right2)
- {
-    int leftSum = 0, rightSum = 0;
-    for (int i = left1; i <= right1; i++) leftSum += coins[i];  //first part
-    for (int j = left2; j <= right2; j++) rightSum += coins[j];    //second part
+
+
+int weight(int coins[], int left1, int right1, int left2, int right2) {
+    int sum1 = 0, sum2 = 0;
+    for (int i = left1; i <= right1; i++) sum1 += coins[i];
+    for (int j = left2; j <= right2; j++) sum2 += coins[j];
     
-    if (leftSum < rightSum) return -1; 
-    if (leftSum > rightSum) return 1;  
-    return 0;                   
+    if (sum1 < sum2) return -1; // left side lighter
+    if (sum1 > sum2) return 1;  // right side lighter
+    return 0;                   // equal
 }
 
-
+// Recursive function to find defective coin
 int findDefective(int coins[], int start, int end) {
-    int n = start  - end+ 1;
+    int n = end - start + 1;
 
-    
+    // only one coin
     if (n == 1) {
         return start; 
     }
 
     int mid = n / 2;
     int leftStart = start;
-    int leftEnd   = start+ mid - 1;
+    int leftEnd   = start + mid - 1;
     int rightStart= start + mid;
-    int rightEnd  =  start+ 2*mid - 1;
+    int rightEnd  = start + 2*mid - 1;
 
     int result = weight(coins, leftStart, leftEnd, rightStart, rightEnd);
 
     if (result == -1) {
+        //left side defective
         return findDefective(coins, leftStart, leftEnd);
-    } 
-    else if (result == 1) {
-        
+    } else if (result == 1) {
+        //right side defective
         return findDefective(coins, rightStart, rightEnd);
-    } 
-    else {
-        
+    } else {
+        // both sides equal
         if (n % 2 == 1) {
-            int oddCoinIdx = end;
-            
-            if (coins[oddCoinIdx] < coins[start])
-                return oddCoinIdx;
+            int oddCoinIndex = end;
+            // Compare odd coin with any known good coin (say coins[l])
+            if (coins[oddCoinIndex] < coins[start])
+                return oddCoinIndex;
         }
-        
+        // No defective coin
         return -1;
     }
 }
@@ -53,18 +53,18 @@ int main() {
     scanf("%d", &n);
 
     int coins[n];
-    printf("Enter coins: ");
+    printf("Enter %d coins: ", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &coins[i]);
     }
 
-    int defectiveIdx = findDefective(coins, 0, n - 1);
+    int defectiveIndex = findDefective(coins, 0, n - 1);
 
-    if (defectiveIdx == -1)
+    if (defectiveIndex == -1)
         printf("No defective coin found. All are perfect.\n");
     else
         printf("Defective (lighter) coin found at index %d (weight = %d).\n",
-               defectiveIdx, coins[defectiveIdx]);
+               defectiveIndex, coins[defectiveIndex]);
 
     return 0;
 }
